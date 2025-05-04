e554f37e67534d7d604ad67c23ff763becca7336
lostofone <grot.artem@gmail.com>
2025-05-04 15:26:30 +0000

    feat: add join-us section layout

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Mission from './MainBlocks/Mission';
 import ActualProjects from './MainBlocks/ActualProjects';
+import JoinUs from './MainBlocks/JoinUs';
 import './Main.css';
 
 function Main() {
@@ -8,6 +9,7 @@ function Main() {
     <div className="main-content">
       <Mission></Mission>
       <ActualProjects></ActualProjects>
+      <JoinUs></JoinUs>
     </div>
   );
 }
