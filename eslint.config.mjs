22a1ab6df993606a413cea80fc6819057b9ab7eb
lostofone <grot.artem@gmail.com>
2025-05-14 17:29:30 +0000

    chore: exclude the vscode directory from the checked eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,10 @@
-import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  eslint.configs.recommended,
+  js.configs.recommended,
+  {
+    ignores: ['**/.vscode/**'],
+  },
   tseslint.configs.recommended
 );
